cd9e4416f8dc6f6b6f234f19dc74a03993998420
alamin <alaminripon5654@gmail.com>
2022-04-06 11:34:13 +0600

    Creating list of tasks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
 
 const Header = ({title}) => {
+  const onClick = () => {
+    console.log('Click')
+  }
   return (
-    <header>
+    <header className='header'>
       <h1>{title}</h1>
+      <Button color= 'green' text='Add' onClick={onClick} />
     </header>
   )
 }
@@ -10,5 +17,14 @@ const Header = ({title}) => {
 Header.defaultProps = {
   title: "Task Tracker"
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+// CSS in js 
+// const headingStyle = {
+//   color: 'blue', backgroundColor: 'black'
+// }
 
 export default Header
